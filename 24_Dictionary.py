82ab30444c3372474d84a8a76c6444270cfa0829
Sviatoslav <s.oseredchuk@gmail.com>
2025-12-02 16:30:02 +0200

    dd concessions program

--- 24_Dictionary.py
+++ 24_Dictionary.py
@@ -6,27 +6,30 @@ capitals = {"USA": "Washington D.C.",
             "UAE": "Abu Dhabi"}
 print(capitals.get("UAE"))
 
-capitals.update({"Germany": "Berlin"})
 
-print(capitals)
-# capitals.pop("Germany") #видалить ключ
-# capitals.popitem() # видалить останній ключ
+print(capitals["UAE"])
+
+# capitals.update({"Germany": "Berlin"})
+#
 # print(capitals)
-# capitals.clear()
-# print(capitals) #Повністю видаляє
-
-keys = capitals.keys() # виводить лише назву ключів
-print(keys)
-for key in capitals.keys():
-    print(key)
-
-values = capitals.values()
-print(values)
-
-items = capitals.items()
-print(items)
-for key, value in items:
-    print(f"The capital of {key} is {value}.")
+# # capitals.pop("Germany") #видалить ключ
+# # capitals.popitem() # видалить останній ключ
+# # print(capitals)
+# # capitals.clear()
+# # print(capitals) #Повністю видаляє
+#
+# keys = capitals.keys() # виводить лише назву ключів
+# print(keys)
+# for key in capitals.keys():
+#     print(key)
+#
+# values = capitals.values()
+# print(values)
+#
+# items = capitals.items()
+# print(items)
+# for key, value in items:
+#     print(f"The capital of {key} is {value}.")
 
 
 
